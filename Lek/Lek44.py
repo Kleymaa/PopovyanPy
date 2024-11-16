bd939c6d16d2a6ff2867d9229283e3278b039c44
Tema <artempopovyan@yandex.ru>
2024-11-16 16:24:37 +0300

    Лекции

---
+++ Lek/Lek44.py
@@ -0,0 +1,55 @@
+             # Позиция 1,  Позиция 2
+# def my_def(fit_name, last_name):
+#     print('Hi!', fit_name, last_name)
+#
+#
+# a = input('Введите имя')
+# b = input('Введите фамилию')
+# my_def(a, b)
+
+
+# def my_sum(a,b):
+#     if a > b:
+#         return a + b
+#     else:
+#         return a - b
+#
+#
+# num1 = int(input("Введи 1 число:"))
+# num2 = int(input("Введи 2 число:"))
+# print('Результат = ', my_sum(num1,num2))
+
+
+# def my_sum(a,b):
+#     return a + b, a * b
+#
+#
+# num1 = int(input("Введи 1 число:"))
+# num2 = int(input("Введи 2 число:"))
+# s, d = my_sum(num1,num2)
+# print('Результат результат суммы= ', s )
+# print('Результат произведения суммы= ', d )
+
+
+# var1 = 'Global'
+#
+#
+# def foo():
+#     global var2, var1
+#     var1 = 'local'
+#     var2 = 'local2'
+#     # print(var1)
+#
+#
+# print(var1)
+# foo()
+# print(var2)
+
+
+# def my_def(*args):
+#     print(args)
+#     return 'True'
+#
+#
+# my_def(1, 2, 'True', 2.36)
+# print(my_def())
