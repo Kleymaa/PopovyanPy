991458bf43a89a7033247d4b7e109cc9b833283d
Tema <artempopovyan@yandex.ru>
2025-05-10 10:43:03 +0300

    Update Pz_13_1.py

--- Pz_13/Pz_13_1.py
+++ Pz_13/Pz_13_1.py
@@ -1,18 +1,25 @@
 #В двумерном списке найти максимальный положительный элемент, кратный 4.
 
-from itertools import chain
-def find_4(matrix):
+from functools import reduce
+
+def find_max_positive_mult_of_4(matrix):
     # Преобразуем матрицу в плоский список
-    flattened = chain.from_iterable(matrix)
-    # Фильтруем элементы: положительные и кратные 4
+    flattened = reduce(lambda acc, row: acc + row, matrix, [])
+
+    # Фильтрация + элем. , кратные 4
     filtered = filter(lambda x: x > 0 and x % 4 == 0, flattened)
-    # Находим максимальный элемент (если таких нет, вернётся None)
-    return max(filtered, default=None)
+
+    #Поиск макс. элемент
+    try:
+        return reduce(lambda a, b: a if a > b else b, filtered)
+    except TypeError:  # если filtered пустой
+        return None
 
 matrix = [
     [1, -8, 12],
-    [5, 16, 0],
-    [9, -4, 8]
+    [4, 16, -3],
+    [0, 5, 24]
 ]
-result = find_4(matrix)
+
+result = find_max_positive_mult_of_4(matrix)
 print(result)
