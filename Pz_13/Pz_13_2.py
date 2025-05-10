9a14dad9d590107d8744594cfcbb7643e5173d7a
Tema <artempopovyan@yandex.ru>
2025-05-10 10:46:53 +0300

    Update Pz_13_2.py

--- Pz_13/Pz_13_2.py
+++ Pz_13/Pz_13_2.py
@@ -1,21 +1,13 @@
 #В двумерном списке все элементы, не лежащие на главной диагонали увеличить в 2
 #раза.
 
-def diagonal(matrix):
-    # Используем map для обработки каждой строки с учётом индекса строки
-    return list(map(
-        lambda row: list(map(
-            lambda item: item[1] * 2 if item[0] != row[0] else item[1],
-            enumerate(row)
-        )),
-        enumerate(matrix)
-    ))
-
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-result = diagonal(matrix)
-for row in result:
+
+Aboba = list(map(lambda a: list(map(lambda b: matrix[a][b] * 2 if a != b else matrix[a][b], range(len(matrix[a])))), range(len(matrix))))
+
+for row in Aboba:
     print(row)
