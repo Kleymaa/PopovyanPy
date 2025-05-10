d3e5c35ac3fc4acdbdf703620812ccc361295125
Tema <artempopovyan@yandex.ru>
2025-05-10 10:27:21 +0300

    Update Pz_12_1.py

--- Pz_12/Pz_12_1.py
+++ Pz_12/Pz_12_1.py
@@ -9,13 +9,27 @@ def sum_last_half(sequence):
     last_half = sequence[half_length:]
     return reduce(lambda x, y: x + y, last_half)
 
-user = input("Введите любые числа через пробел: ")
-numbers = list(map(int, user.split()))
-result = sum_last_half(numbers)
-print("Сумма второй половины (введённый список):", result)
+user_input = input("Введите последовательность целых чисел через пробел: ").strip()
 
+#Генерация 
 random_length = random.randint(5, 10)
 random_numbers = [random.randint(1, 100) for _ in range(random_length)]
 random_result = sum_last_half(random_numbers)
 print("\nСлучайный список:", random_numbers)
 print("Сумма второй половины (случайный список):", random_result)
+
+#Обработка пользовательского 
+if user_input == "":
+    print("\nИспользуется случайный список, так как ввод пуст.")
+else:
+    try:
+        user_numbers = list(map(int, user_input.split()))
+        if len(user_numbers) == 0:
+            print("Ошибка: не введено ни одного числа!")
+        else:
+            user_result = sum_last_half(user_numbers)
+            print("\nСумма второй половины списка:", user_result)
+    except ValueError:
+        print("Ошибка: введите только целые числа, разделенные пробелами!")
+
+
