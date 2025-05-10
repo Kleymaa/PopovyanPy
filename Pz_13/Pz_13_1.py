9bd57a9b6ed487ae319a650d2caab429212e5fa2
Tema <artempopovyan@yandex.ru>
2025-05-10 14:23:35 +0300

    Update Pz_13_1.py

--- Pz_13/Pz_13_1.py
+++ Pz_13/Pz_13_1.py
@@ -2,17 +2,17 @@
 
 from functools import reduce
 
-def find_max_positive_mult_of_4(matrix):
+def find_max(matrix):
     # Преобразуем матрицу в плоский список
     flattened = reduce(lambda acc, row: acc + row, matrix, [])
 
-    # Фильтрация + элем. , кратные 4
+    # Филь. + элем. кратные 4
     filtered = filter(lambda x: x > 0 and x % 4 == 0, flattened)
 
     #Поиск макс. элемент
     try:
         return reduce(lambda a, b: a if a > b else b, filtered)
-    except TypeError:  # если filtered пустой
+    except TypeError:
         return None
 
 matrix = [
@@ -21,5 +21,5 @@ matrix = [
     [0, 5, 24]
 ]
 
-result = find_max_positive_mult_of_4(matrix)
+result = find_max(matrix)
 print(result)
